adc7cea29d2af9459285d70455c1d58398c76e78
Simon Knupfer <simon@knupfer.eu>
2017-09-13 12:01:08 +0200

    Pfad rausbekommen fuer application.config laden

--- HackZurich.JavaBackend/src/main/java/ch/raiffeisen/hackzurich/controller/DemoController.java
+++ HackZurich.JavaBackend/src/main/java/ch/raiffeisen/hackzurich/controller/DemoController.java
@@ -7,6 +7,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by simon on 20.06.2017.
@@ -22,6 +25,23 @@ public class DemoController {
     @RequestMapping(path = "/", method= RequestMethod.GET)
     public Iterable<Person> listPersonen() {
         return personRepository.listPerson();
+    }
+
+
+    @RequestMapping("/userhome")
+    public String userhome() {
+        return System.getProperty("user.dir");
+    }
+
+    @RequestMapping("/files")
+    public List<String> files() {
+        List<String> result = new ArrayList<>();
+        File file  = new File(System.getProperty("user.dir"));
+        File[] files = file.listFiles();
+        for (File file1 : files) {
+            result.add(file1.getAbsolutePath());
+        }
+        return result;
     }
 
 }
